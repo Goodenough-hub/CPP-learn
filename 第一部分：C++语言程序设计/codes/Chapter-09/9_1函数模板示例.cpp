46838adcf4c0b5c259342695460e0faa0f90d295
Goodenough <2323342501@qq.com>
2023-02-14 17:23:17 +0800

    section1 chapter9 finished

---
+++ 第一部分：C++语言程序设计/codes/Chapter-09/9_1函数模板示例.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+template<class T> // 定义函数模板
+void outputArray(const T *array, int count){
+    for(int i = 0; i < count; i ++)
+        cout << array[i] << " "; // 如果数组对象时类的对象，需要该对象所属类重载了流插入运算符"<<"
+    cout << endl;
+}
+
+int main(){
+    const int A_COUNT = 8, B_COUNT = 8, C_COUNT = 20;
+    int a[A_COUNT] = {1, 2, 3, 4, 5, 6, 7, 8};
+    double b[B_COUNT] = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8};
+    char c[C_COUNT] = "Welcom!";
+
+    cout << " a array contains:" << endl;
+    outputArray(a, A_COUNT);
+    cout << "b array contains:" << endl;
+    outputArray(b, B_COUNT);
+    cout << "c array contains:" << endl;
+    outputArray(c, C_COUNT);
+    return 0;
+}
